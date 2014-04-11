c13ac8a64074e1621a67f2bb2ca7baa0691799da
Jordan Saxe <jordansaxe15@gmail.com>
2014-04-10 21:00:51 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skyfall::Application.config.secret_key_base = 'c79a7f8167cac11cdd7712ad535983d714cc2dab0061b736f44b63b6b681fceaf68dbd7fe048b38c41ed9908f06c058831def4dbffcdd9782ed79cefd7df9da9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Skyfall::Application.config.session_store :cookie_store, key: '_skyfall_session'
