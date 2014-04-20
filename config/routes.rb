c9703e4a51d9fa23389d47c7565ac5e073140169
Jordan Saxe <jordansaxe15@gmail.com>
2014-04-20 11:52:15 -0400

    Add devise for user authentication and bootstrap for styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Skyfall::Application.routes.draw do
+  devise_for :users
   resources :trips
+
+  root to: "trips#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
